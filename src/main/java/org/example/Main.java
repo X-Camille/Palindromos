f71e3b69aed70fab026de895ce48c100f090aa8b
miel <m.ferrada09@ufromail.cl>
2023-09-07 15:52:01 -0300

    commit de miel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("Amo a melito");
+
+        System.out.println(esPalindromo("osos"));
     }
+
+    public static boolean esPalindromo(String cadena) {
+        int largo = cadena.length();
+        char [] reverso = new char[largo];
+        char[] separada = cadena.toCharArray();
+        for (int i=1; i<=largo; i++){
+            reverso[i-1]=separada[largo-i];
+        }
+        String resultado = new String(reverso);
+        return resultado.equals(cadena);
+    }
+
 }
