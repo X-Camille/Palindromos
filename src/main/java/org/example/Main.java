b3e59db991860aef1892ef7738a1a0d25aa20f78
Xiao <z.xiao01@ufromail.cl>
2023-09-11 20:28:17 -0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String palabra = "La tele letal";
+        boolean esPalindromo = esPalindromo(palabra);
+        System.out.println("¿Es palíndromo? " + esPalindromo);
 
-        System.out.println("Hello world!");
     }
+    public static boolean esPalindromo(String palabraIngresada) {
+        palabraIngresada = palabraIngresada.replace(" ", "");
+        StringBuilder stringBuilder = new StringBuilder(palabraIngresada);
+        String palabraInvertida = stringBuilder.reverse().toString();
+        return palabraIngresada.equalsIgnoreCase(palabraInvertida); // "caddena.equalsIgnoreCase" sirve para comparara 2 cadenas de textos ingnorando las diferencai demayusculas y minusuclas
+    }
+
 }
+
